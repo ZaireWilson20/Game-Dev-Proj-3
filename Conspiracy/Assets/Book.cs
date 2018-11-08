396744855b8c888527c22cc53e75de4bdee7d5c1
Michael Smith <sirforcer@gmail.com>
2018-11-08 18:45:19 -0500

    Added Person and started animations

---
+++ Conspiracy/Assets/Book.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Book : MonoBehaviour {
+    public Animator bookanim;
+    public GameObject bookStuff; 
+	// Use this for initialization
+	void Start () {
+        bookanim = bookStuff.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void bookOpen()
+    {
+        bookanim.SetBool("openBook", true);
+    }
+}
