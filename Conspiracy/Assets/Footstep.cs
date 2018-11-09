05b0e739295de51e98a4f0a88f0b76b36d070c7f
Ben-Wolf <bwolfgns@gmail.com>
2018-11-08 19:28:10 -0500

    Implemented music with audiomanager

---
+++ Conspiracy/Assets/Footstep.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footstep : MonoBehaviour {
+
+    CharacterController cc;
+	// Use this for initialization
+
+	void Start () {
+        cc = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<AudioSource>().Play();
+        if (cc.velocity.magnitude > 2f &&
+            GetComponent<AudioSource>().isPlaying == false) {
+            GetComponent<AudioSource>().Play();
+        }
+	}
+}
