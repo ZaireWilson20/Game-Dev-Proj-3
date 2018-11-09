a1f6456dc756b005c379fe08eb06b0ec934f4253
Anthony Veschi <anthonyveschi@gmail.com>
2018-11-09 01:35:56 -0500

    Scene for Puzzle#2

---
+++ Conspiracy/Assets/Scripts/PuzzleButtonScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzleButtonScript : MonoBehaviour {
+
+    public bool selected = false;
+    Color selectedColor = new Color(1f, 0f, 0f);
+    Color notSelectedColor = new Color(1f, 1f, 1f);
+    PuzzleManagerScript managerScript;
+
+    Image image;
+
+    private void Start()
+    {
+        managerScript = GameObject.Find("PuzzleManager").GetComponent<PuzzleManagerScript>();
+
+        image = gameObject.GetComponent<Image>();
+    }
+
+    public void Select()
+    {
+        image.color = selectedColor;
+        selected = true;
+        managerScript.Selected();
+    }
+
+    public void Deselect()
+    {
+        image.color = notSelectedColor;
+        selected = false;
+    }
+}
---
+++ Conspiracy/Assets/Scripts/PuzzleManagerScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PuzzleManagerScript : MonoBehaviour {
+
+    int numSelected = 0;
+
+    PuzzleButtonScript button0;
+    PuzzleButtonScript button1;
+    PuzzleButtonScript button2;
+    PuzzleButtonScript button3;
+    PuzzleButtonScript button4;
+    PuzzleButtonScript button5;
+    PuzzleButtonScript button6;
+    PuzzleButtonScript button7;
+
+    void Start()
+    {
+        button0 = GameObject.Find("PuzzleButton0").GetComponent<PuzzleButtonScript>();
+        button1 = GameObject.Find("PuzzleButton1").GetComponent<PuzzleButtonScript>();
+        button2 = GameObject.Find("PuzzleButton2").GetComponent<PuzzleButtonScript>();
+        button3 = GameObject.Find("PuzzleButton3").GetComponent<PuzzleButtonScript>();
+        button4 = GameObject.Find("PuzzleButton4").GetComponent<PuzzleButtonScript>();
+        button5 = GameObject.Find("PuzzleButton5").GetComponent<PuzzleButtonScript>();
+        button6 = GameObject.Find("PuzzleButton6").GetComponent<PuzzleButtonScript>();
+        button7 = GameObject.Find("PuzzleButton7").GetComponent<PuzzleButtonScript>();
+
+        buttons[0] = button0;
+        buttons[1] = button1;
+        buttons[2] = button2;
+        buttons[3] = button3;
+        buttons[4] = button4;
+        buttons[5] = button5;
+        buttons[6] = button6;
+        buttons[7] = button7;
+    }
+
+    PuzzleButtonScript[] buttons = new PuzzleButtonScript[8];
+
+    public void Selected()
+    {
+        numSelected++;
+        if (numSelected == 2)
+        {
+            numSelected = 0;
+            if (button0.selected && button6.selected)
+            {
+                //send a message to notebook
+                //set a flag to pull up some dialogue
+                SceneManager.LoadScene("Map");
+            }
+            else
+            {
+                foreach (PuzzleButtonScript script in buttons)
+                {
+                    script.Deselect();
+                }
+            }
+        }
+    }
+}
